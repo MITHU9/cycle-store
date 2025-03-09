5e33098426d18d1d37e17ef5c7921be4fe2c9909
mahmudul35 <mahmudul.cse.pust@gmail.com>
2025-03-10 00:44:48 +0600

    added endpoint for post product

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
-const express = require("express");
-const cors = require("cors");
+import cors from "cors";
+import express from "express";
+import productRoute from "../backend/routes/productRoute.js";
+import connectDB from "./config/db.js";
 const app = express();
-require("dotenv").config();
-const port = process.env.PORT || 3000;
 
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-
+connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/products", productRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
